671cb39fa8cf05ced960c1f74acca58831d2d1b5
przeCze <prze.cze@gmail.com>
2014-12-29 17:06:55 +0100

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,15 +1,17 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
   """ kSDJ  """
   def setUp(self):
     self.browser = webdriver.Firefox()
+    self.browser.refresh()
     self.browser.implicitly_wait(3)
 
   def tearDown(self):
+    self.browser.refresh()
     self.browser.quit()
 
   def check_for_row_in_list_table(self, row_text):
